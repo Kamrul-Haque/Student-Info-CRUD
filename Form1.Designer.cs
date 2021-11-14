fa2c85a91986a10bc73b09e5a51b7128b60b3fab
Kamrul-Haque <kamrul35-255@diu.edu.bd>
2021-11-14 11:34:59 +0600

    Backing Up

---
+++ Form1.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace Student_Information
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tbxID = new System.Windows.Forms.TextBox();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.tbxDept = new System.Windows.Forms.TextBox();
+            this.tbxDob = new System.Windows.Forms.TextBox();
+            this.tbxPhone = new System.Windows.Forms.TextBox();
+            this.tbxEmail = new System.Windows.Forms.TextBox();
+            this.tbxPresentA = new System.Windows.Forms.TextBox();
+            this.tbxPermanentA = new System.Windows.Forms.TextBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnDeleteA = new System.Windows.Forms.Button();
+            this.btnUpdateDB = new System.Windows.Forms.Button();
+            this.cbxGender = new System.Windows.Forms.ComboBox();
+            this.cbxBg = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Department:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Gender:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Blood Group:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 235);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Date of Birth:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 281);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Phone:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 333);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Email:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 382);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(87, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Present Address:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 428);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(102, 13);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Permanent Address:";
+            // 
+            // tbxID
+            // 
+            this.tbxID.Location = new System.Drawing.Point(117, 6);
+            this.tbxID.Name = "tbxID";
+            this.tbxID.Size = new System.Drawing.Size(100, 20);
+            this.tbxID.TabIndex = 10;
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(117, 51);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(100, 20);
+            this.tbxName.TabIndex = 11;
+            // 
+            // tbxDept
+            // 
+            this.tbxDept.Location = new System.Drawing.Point(117, 95);
+            this.tbxDept.Name = "tbxDept";
+            this.tbxDept.Size = new System.Drawing.Size(100, 20);
+            this.tbxDept.TabIndex = 12;
+            // 
+            // tbxDob
+            // 
+            this.tbxDob.Location = new System.Drawing.Point(117, 232);
+            this.tbxDob.Name = "tbxDob";
+            this.tbxDob.Size = new System.Drawing.Size(100, 20);
+            this.tbxDob.TabIndex = 15;
+            // 
+            // tbxPhone
+            // 
+            this.tbxPhone.Location = new System.Drawing.Point(117, 278);
+            this.tbxPhone.Name = "tbxPhone";
+            this.tbxPhone.Size = new System.Drawing.Size(100, 20);
+            this.tbxPhone.TabIndex = 16;
+            // 
+            // tbxEmail
+            // 
+            this.tbxEmail.Location = new System.Drawing.Point(117, 330);
+            this.tbxEmail.Name = "tbxEmail";
+            this.tbxEmail.Size = new System.Drawing.Size(100, 20);
+            this.tbxEmail.TabIndex = 17;
+            // 
+            // tbxPresentA
+            // 
+            this.tbxPresentA.Location = new System.Drawing.Point(117, 379);
+            this.tbxPresentA.Name = "tbxPresentA";
+            this.tbxPresentA.Size = new System.Drawing.Size(100, 20);
+            this.tbxPresentA.TabIndex = 18;
+            // 
+            // tbxPermanentA
+            // 
+            this.tbxPermanentA.Location = new System.Drawing.Point(117, 425);
+            this.tbxPermanentA.Name = "tbxPermanentA";
+            this.tbxPermanentA.Size = new System.Drawing.Size(100, 20);
+            this.tbxPermanentA.TabIndex = 19;
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(234, 12);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(554, 361);
+            this.dgv.TabIndex = 20;
+            this.dgv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellClick);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnInsert.Location = new System.Drawing.Point(234, 379);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 66);
+            this.btnInsert.TabIndex = 21;
+            this.btnInsert.Text = "INSERT";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnModify
+            // 
+            this.btnModify.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModify.Location = new System.Drawing.Point(315, 379);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(75, 66);
+            this.btnModify.TabIndex = 22;
+            this.btnModify.Text = "MODIFY";
+            this.btnModify.UseVisualStyleBackColor = true;
+            this.btnModify.Click += new System.EventHandler(this.btnModify_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(396, 379);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 66);
+            this.btnDelete.TabIndex = 23;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnDeleteA
+            // 
+            this.btnDeleteA.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeleteA.ForeColor = System.Drawing.Color.Red;
+            this.btnDeleteA.Location = new System.Drawing.Point(477, 379);
+            this.btnDeleteA.Name = "btnDeleteA";
+            this.btnDeleteA.Size = new System.Drawing.Size(117, 66);
+            this.btnDeleteA.TabIndex = 24;
+            this.btnDeleteA.Text = "DELETE ALL!";
+            this.btnDeleteA.UseVisualStyleBackColor = true;
+            this.btnDeleteA.Click += new System.EventHandler(this.btnDeleteA_Click);
+            // 
+            // btnUpdateDB
+            // 
+            this.btnUpdateDB.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdateDB.ForeColor = System.Drawing.Color.Blue;
+            this.btnUpdateDB.Location = new System.Drawing.Point(600, 379);
+            this.btnUpdateDB.Name = "btnUpdateDB";
+            this.btnUpdateDB.Size = new System.Drawing.Size(188, 66);
+            this.btnUpdateDB.TabIndex = 25;
+            this.btnUpdateDB.Text = "UPDATE DATABASE";
+            this.btnUpdateDB.UseVisualStyleBackColor = true;
+            this.btnUpdateDB.Click += new System.EventHandler(this.btnUpdateDB_Click);
+            // 
+            // cbxGender
+            // 
+            this.cbxGender.FormattingEnabled = true;
+            this.cbxGender.Items.AddRange(new object[] {
+            "MALE",
+            "FEMALE",
+            "OTHER"});
+            this.cbxGender.Location = new System.Drawing.Point(117, 140);
+            this.cbxGender.Name = "cbxGender";
+            this.cbxGender.Size = new System.Drawing.Size(100, 21);
+            this.cbxGender.TabIndex = 26;
+            // 
+            // cbxBg
+            // 
+            this.cbxBg.FormattingEnabled = true;
+            this.cbxBg.Items.AddRange(new object[] {
+            "A+Ve",
+            "B+Ve",
+            "O+Ve",
+            "A-Ve",
+            "B-Ve",
+            "O-Ve",
+            "AB+Ve",
+            "AB-Ve"});
+            this.cbxBg.Location = new System.Drawing.Point(117, 185);
+            this.cbxBg.Name = "cbxBg";
+            this.cbxBg.Size = new System.Drawing.Size(100, 21);
+            this.cbxBg.TabIndex = 27;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbxBg);
+            this.Controls.Add(this.cbxGender);
+            this.Controls.Add(this.btnUpdateDB);
+            this.Controls.Add(this.btnDeleteA);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.tbxPermanentA);
+            this.Controls.Add(this.tbxPresentA);
+            this.Controls.Add(this.tbxEmail);
+            this.Controls.Add(this.tbxPhone);
+            this.Controls.Add(this.tbxDob);
+            this.Controls.Add(this.tbxDept);
+            this.Controls.Add(this.tbxName);
+            this.Controls.Add(this.tbxID);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Student Information";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox tbxID;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.TextBox tbxDept;
+        private System.Windows.Forms.TextBox tbxDob;
+        private System.Windows.Forms.TextBox tbxPhone;
+        private System.Windows.Forms.TextBox tbxEmail;
+        private System.Windows.Forms.TextBox tbxPresentA;
+        private System.Windows.Forms.TextBox tbxPermanentA;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnDeleteA;
+        private System.Windows.Forms.Button btnUpdateDB;
+        private System.Windows.Forms.ComboBox cbxGender;
+        private System.Windows.Forms.ComboBox cbxBg;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Student_Information
+{
+    public partial class Form1 : Form
+    {
+        readonly SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=Student_Info;Integrated Security=true");
+        SqlDataAdapter adapter;
+        DataTable table;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            adapter = new SqlDataAdapter("SELECT * FROM info", connection);
+            adapter.FillSchema(table, SchemaType.Source);
+            adapter.Fill(table);
+            dgv.DataSource = table;
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataRow row = table.NewRow();
+                row["ID"] = Convert.ToInt32(tbxID.Text);
+                row["Name"] = tbxName.Text;
+                row["Department"] = tbxDept.Text;
+                row["Gender"] = cbxGender.Text;
+                row["Blood_Group"] = cbxBg.Text;
+                row["Date_of_Birth"] = tbxDob.Text;
+                row["Phone"] = tbxPhone.Text;
+                row["Email"] = tbxEmail.Text;
+                row["Present_Address"] = tbxPresentA.Text;
+                row["Permanent_Address"] = tbxPermanentA.Text;
+                table.Rows.Add(row);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnUpdateDB_Click(object sender, EventArgs e)
+        {
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+            adapter.Update(table);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataRow row = table.Rows[dgv.CurrentRow.Index];
+                table.Rows.Remove(row);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnModify_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataRow row = table.Rows[dgv.CurrentRow.Index];
+                if(row["ID"].Equals( Convert.ToInt32(tbxID.Text)))
+                {
+                    row["Name"] = tbxName.Text;
+                    row["Department"] = tbxDept.Text;
+                    row["Gender"] = cbxGender.Text;
+                    row["Blood_Group"] = cbxBg.Text;
+                    row["Date_of_Birth"] = tbxDob.Text;
+                    row["Phone"] = tbxPhone.Text;
+                    row["Email"] = tbxEmail.Text;
+                    row["Present_Address"] = tbxPresentA.Text;
+                    row["Permanent_Address"] = tbxPermanentA.Text;
+                }
+                else
+                {
+                    MessageBox.Show("CAN'T CHANGE THE STUDENT ID!");
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDeleteA_Click(object sender, EventArgs e)
+        {
+            table.Rows.Clear();
+        }
+
+        private void dgv_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            tbxID.Text = dgv.CurrentRow.Cells[0].Value.ToString();
+            tbxName.Text = dgv.CurrentRow.Cells[1].Value.ToString();
+            tbxDept.Text = dgv.CurrentRow.Cells[2].Value.ToString();
+            cbxGender.Text = dgv.CurrentRow.Cells[3].Value.ToString();
+            cbxBg.Text = dgv.CurrentRow.Cells[4].Value.ToString();
+            tbxDob.Text = dgv.CurrentRow.Cells[5].Value.ToString();
+            tbxPhone.Text = dgv.CurrentRow.Cells[6].Value.ToString();
+            tbxEmail.Text = dgv.CurrentRow.Cells[7].Value.ToString();
+            tbxPresentA.Text = dgv.CurrentRow.Cells[8].Value.ToString();
+            tbxPermanentA.Text = dgv.CurrentRow.Cells[9].Value.ToString();
+        }
+    }
+}
